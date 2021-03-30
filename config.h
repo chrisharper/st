3f2a4edfeb6a7f02827c215c94d5dec88919203e
Chris Harper <charper+git@charper.co.uk>
2021-03-30 10:41:08 +0100

    Gruvbox based colours

--- config.h
+++ config.h
@@ -5,7 +5,7 @@
  *
  * font: see http://freedesktop.org/software/fontconfig/fontconfig-user.html
  */
-static char *font = "Liberation Mono:pixelsize=12:antialias=true:autohint=true";
+static char *font = "Source Code Pro:size=14:antialias=true:hinting=true";
 static int borderpx = 2;
 
 /*
@@ -96,30 +96,31 @@ unsigned int tabspaces = 8;
 /* Terminal colors (16 first used in escape sequence) */
 static const char *colorname[] = {
 	/* 8 normal colors */
-	"black",
-	"red3",
-	"green3",
-	"yellow3",
-	"blue2",
-	"magenta3",
-	"cyan3",
-	"gray90",
+	"#272727",
+	"#cc231c",
+	"#989719",
+	"#d79920",
+	"#448488",
+	"#b16185",
+	"#689d69",
+	"#a89983",
 
 	/* 8 bright colors */
-	"gray50",
-	"red",
-	"green",
-	"yellow",
-	"#5c5cff",
-	"magenta",
-	"cyan",
-	"white",
+	"#928373",
+	"#fb4833",
+	"#b8ba25",
+	"#fabc2e",
+	"#83a597",
+	"#d3859a",
+	"#8ec07b",
+	"#ebdbb2",
 
 	[255] = 0,
 
 	/* more colors can be added after 255 to use with DefaultXX */
 	"#cccccc",
 	"#555555",
+	"#ebdbb2"
 };
 
 
@@ -129,7 +130,7 @@ static const char *colorname[] = {
  */
 unsigned int defaultfg = 7;
 unsigned int defaultbg = 0;
-static unsigned int defaultcs = 256;
+static unsigned int defaultcs = 258;
 static unsigned int defaultrcs = 257;
 
 /*
