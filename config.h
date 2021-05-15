479803f97d6e6064fafa168edfc5003ec2cb3179
Chris Harper <charper+git@charper.co.uk>
2021-05-15 15:45:26 +0000

    New Font

--- config.h
+++ config.h
@@ -5,7 +5,7 @@
  *
  * font: see http://freedesktop.org/software/fontconfig/fontconfig-user.html
  */
-static char *font = "Source Code Pro:size=14:antialias=true:hinting=true";
+static char *font = "DejaVu Sans Mono:size=14:antialias=true:hinting=true";
 static int borderpx = 2;
 
 /*
